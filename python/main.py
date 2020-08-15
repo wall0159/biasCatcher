b2e1bdd608adc7db6b5b9f060383cb5ebd6ab12a
Angus Wallace <angus.wallace@flinders.edu.au>
2020-08-15 12:08:33 +0930

    first commit

---
+++ python/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == "__main__":
+    app.run(debug=True)
